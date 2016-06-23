02b782cd56bbe2457e783502d95846757727687b
maorros <maorros@gmail.com>
2016-06-23 18:04:17 +0300

    Maor: added Agent class

---
+++ agent/Agent.py
@@ -0,0 +1,16 @@
+from tangrams import *
+import json
+
+class Agent(self):
+    def __init__(self):
+        self.solver = Solver()
+
+    def solve_task(self, json_str_task):
+        task = Task()
+        task.create_from_json(json_str_task)
+        self.solver.set_available_pieces(task)
+        seq = self.solver.run_task(task, stop=True)
+        # TODO convert seq to json str, for now just return the moves of the task
+        return json_str_task
+
+
